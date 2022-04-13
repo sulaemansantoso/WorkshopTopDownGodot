3500c7509ecb7d8c33cf45c6fc420454ec82aeb1
Sulaeman Santoso <sulaeman.santoso@gmail.com>
2022-04-13 22:06:07 +0700

    pertemuan 2

--- Script/ContohNode.gd
+++ Script/ContohNode.gd
@@ -1,12 +1,29 @@
 extends Node2D
-var health_point : int = 100
-var array_bilangan = []
 
-func fungsi_saya(bil1 : int , bil2 : int) -> int :
-	print(str(bil1+ bil2))
-	return bil1 + bil2
+export var speed = 3
 
 func _ready():
-	var hasil = fungsi_saya(2, 3)
-	print(hasil)
+	var tomat_node = get_node("Tomat")
+	tomat_node.scale = Vector2(0.3,0.3)
+	pass
+	
 
+func _process(delta):
+#	var character = get_node("Character")
+	var character = $Character
+	
+	if (Input.is_action_pressed("ui_left")):
+		character.play("Kiri")
+		$Character.position.x -= speed
+	if (Input.is_action_pressed("ui_right")):
+		$Character.play("Kanan")
+		$Character.position.x += speed
+	if (Input.is_action_pressed("ui_down")):
+		$Character.play("Bawah")
+		$Character.position.y += speed
+	if (Input.is_action_pressed("ui_up")):
+		$Character.play("Atas")
+		$Character.position.y -= speed
+		
+	
+	pass
